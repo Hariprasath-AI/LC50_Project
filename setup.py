cf06039a3274cd202ffbb25b142e86af7fdde5ce
HariprasathGurusamy <hariprasath167@gmail.com>
2023-06-23 15:49:42 +0530

    dataingestion

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup 
+
+setup(
+name = 'Prediction of LC50 value',
+version = '1.0',
+author = 'Hariprasath',
+author_email = 'hariprasath167@gmail.com',
+packages = find_packages()
+)
