bc0013b89a268d085abff1da6d0241af8c9060d8
Hariprasath-AI <hariprasath167@gmail.com>
2023-06-25 13:24:15 +0530

    push

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -102,19 +102,20 @@ class DataTransformation:
     def dimensionality_reduction():
         data=DataTransformation.handling_outlier()
         threshold=0.85 # We, set a threshold of 0.85. So, that the feature is removed above the threshold.
+        temp_data = data.drop(['LC50'], axis=1)
         corr_columns = set() # Here, the data structure 'set()' is used avoid the duplicate column names.
-        corr_matrix = data.corr() # object.corr() returns the correlation matrix of the dataset.
-        corr_matrix = corr_matrix.remove(['LC50'])
-            for i in range(len(corr_matrix.columns)):
-                for j in range(i):
-                    # If the correlation is greater than threshold, the column name is added to the set 'corr_columns'.
-                    if corr_matrix.iloc[i,j] > threshold:  
-                        column_name = corr_matrix.columns[i]
-                        corr_columns.add(column_name)
-        data.remove(list(corr_columns), axis=1, inplace=True)
+        corr_matrix = temp_data.corr() # object.corr() returns the correlation matrix of the dataset.
+        for i in range(len(corr_matrix.columns)):
+            for j in range(i):
+                # If the correlation is greater than threshold, the column name is added to the set 'corr_columns'.
+                if corr_matrix.iloc[i,j] > threshold:  
+                    column_name = corr_matrix.columns[i]
+                    corr_columns.add(column_name)
+        data.drop(list(corr_columns), axis=1, inplace=True)
         print(corr_columns)
+        logging.info("[data_transformation.py] Dimensionality reduction successfully completed.")
         return data
-        
+
     # This function splits the data into train and test set for model training purpose.
     def train_test_splitting():
         #data=DataTransformation.dimensionality_reduction()
@@ -125,7 +126,7 @@ class DataTransformation:
             train_data.to_csv('./data/train/train.csv')
             test_data.to_csv('./data/test/test.csv')
         except:
-            continue
+            pass
         logging.info("[data_transformation.py] Splitting data into train and test set is done successfully.")
         logging.info("Data Transformation is completed successfully")
         
