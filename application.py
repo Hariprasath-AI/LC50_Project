f06c721056ca671756a272f9c88097d441167766
Hariprasath-AI <hariprasath167@gmail.com>
2024-11-06 21:08:23 +0530

    new

--- application.py
+++ application.py
@@ -35,4 +35,4 @@ def make_predictions():
 
 # This main method is called while running this application
 if __name__ == '__main__':
-    application.run(host='0.0.0.0')
+    application.run(host='0.0.0.0', port=3000)
